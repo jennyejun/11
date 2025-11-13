d130d05405a73107b54e783bfcf52441358c5111
jennyejun <aprilhan@sookmyung.ac.kr>
2025-11-13 15:47:53 +0900

    Update Week11.c

--- Project11/Project11/Week11.c
+++ Project11/Project11/Week11.c
@@ -1,21 +1,15 @@
 #include <stdio.h>
 
 int main(void) {
-	int i, j;
-	int sum = 0;
-	int grade[5];
+	int i = 100;
+	int* p = &i;
+	int** q = &p;
 
-	for (i = 0; i < 5; i++) {
-		printf("grade[%i] = ", i);
-		scanf_s("%d", &grade[i]);
-	}
+	*p = 200;
+	printf("i = %d, *p = %d, **q = %d\n", i, *p, **q);
 
-	for (j = 0; j < 5; j++) {
-		printf("grade[%i] = %i\n", j, *(grade+j));
-		sum = sum + *(grade + j);
-	}
-
-	printf("average: %d\n", sum/5);
+	**q = 300;
+	printf("i = %d, *p = %d, **q = %d\n", i, *p, **q);
 
 	return 0;
 }
