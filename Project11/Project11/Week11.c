3da39ba838b3f7576b5dd34dc37ba4d583414324
jennyejun <aprilhan@sookmyung.ac.kr>
2025-11-13 15:33:13 +0900

    Update Week11.c

--- Project11/Project11/Week11.c
+++ Project11/Project11/Week11.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 
+void swap(int *x, int *y) {
+	int temp;
+
+	temp = *x;
+	*x = *y;
+	*y = temp;
+}
+
 int main(void) {
-	int i = 300;
-
-	int *pi = &i;
-	char *pc = &i;
-
-	printf("%i, %i, %i\n", i, *pi, *pc);
+	int a = 3;
+	int b = 5;
+	swap(&a, &b);
+	printf("a:%i, b:%i\n", a, b);
 
 	return 0;
 }
